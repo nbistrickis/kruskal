5368db2a9009a63a374411e8fc40b8e30136a372
nerijus <n.bistrickis@gmail.com>
2014-05-01 21:02:42 +0300

    Main part of Kruskal's algorithm pseudoce initialized (only used functions 'make_set', 'find_set', 'union' are still missing)

--- kruskal.py
+++ kruskal.py
@@ -1,12 +1,23 @@
 def make_set(vertice):
     pass
 
-def find(vertice):
+def find_set(vertice):
     pass
 
 def union(vertice1, vertice2):
     pass
 
 def kruskal(graph):
-    pass
+    mst = set()
+    for vertice in graph['vertices']:
+        make_set(vertice)
 
+    edges = list(graph['edges'])
+    edges.sort()
+    for edge in edges:
+        weight, u, v = edge
+        if find_set(u) != find_set(v):
+            mst.add(edge)
+            union(u, v)
+
+    return mst
